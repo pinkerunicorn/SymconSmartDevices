577c4859a9ed5a5f264e330d58b943c8ba7b8546
Florian Graßinger <grassinger@ubyte.gmbh>
2026-08-07 13:18:22 +0200

    debug: use sys_get_temp_dir

--- GardenaGateway/module.php
+++ GardenaGateway/module.php
@@ -270,11 +270,11 @@ class GardenaGateway extends IPSModuleStrict
         }
 
         if ($httpCode >= 200 && $httpCode < 300) {
-            @file_put_contents('/tmp/api_dump.txt', "HTTP $httpCode\n$response");
+            @file_put_contents(sys_get_temp_dir() . '/api_dump.txt', "HTTP $httpCode\n$response");
             return json_decode($response, true);
         }
 
-        @file_put_contents('/tmp/api_dump.txt', "HTTP $httpCode\n$response");
+        @file_put_contents(sys_get_temp_dir() . '/api_dump.txt', "HTTP $httpCode\n$response");
         $this->SLogError("API Request failed (HTTP $httpCode): $response");
         return false;
     }
