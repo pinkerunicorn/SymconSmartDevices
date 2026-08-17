7cbdd41245cfbe0c6a3753aee38e3bde59d7479e
Florian Graßinger <grassinger@ubyte.gmbh>
2026-08-17 16:48:45 +0200

    fix: Suppress property warning on race condition in DeviceAvailability trait

--- libs/Trait_DeviceAvailability.php
+++ libs/Trait_DeviceAvailability.php
@@ -77,7 +77,8 @@ if (!trait_exists('DeviceAvailability_Trait')) {
 
             // Property für Alarm-Priorität (0=Low, 1=Medium, 2=High, -1=kein Alarm)
             // RegisterPropertyInteger ist idempotent in Symcon – kein Existenz-Check nötig
-            $this->RegisterPropertyInteger('AvailabilityAlarmPriority', 1);
+            @$this->RegisterPropertyInteger('AvailabilityAlarmPriority', 1);
+
 
             // Set initial state to true (Online) on creation so newly created variables don't falsely trigger offline alarms
             if ($this->GetValue('DeviceAvailable') === false && time() - IPS_GetVariable($this->GetIDForIdent('DeviceAvailable'))['VariableUpdated'] > 31536000) {
